2c207d8c406710238c628c1b21f145078e2c85c3
Halil <halilski1979@gmail.com>
2025-02-20 10:56:41 +0200

    rwer

---
+++ Urok-31-32-If_Else/Solution1/P04 - Pravoagalnik - LicePer/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P04___Pravoagalnik___LicePer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a=double.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            double b=double.Parse(Console.ReadLine());
+
+            if (a>0 && b>0)
+            {
+                double s = a * b;
+                double p = 2 * a + 2 * b;
+
+                Console.WriteLine($"Liceto e {s}");
+                Console.WriteLine($"Perimetara e {p}");
+            }
+            else
+            {
+                Console.WriteLine($"Error!");
+            }
+        }
+    }
+}
