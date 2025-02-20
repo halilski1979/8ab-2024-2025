4498bda29820faadd136ebc296d38a3e6d38a7f2
Halil <halilski1979@gmail.com>
2025-02-20 10:45:55 +0200

    fdewa

---
+++ Urok-31-32-If_Else/Solution1/P03 - Kvadrat-Perimetar/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P03___Kvadrat_Perimetar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Лице и периметър на квадрат
+            Console.Write("a=");
+            int a=int.Parse(Console.ReadLine());
+
+            if (a>0)
+            {
+                int s = a * a;
+                int p = 4 * a;
+
+                Console.WriteLine($"S={s}");
+                Console.WriteLine($"P={p}");
+            }
+            else
+            {
+                Console.WriteLine("Greshka vav dannite!");
+            }
+
+        }
+    }
+}
